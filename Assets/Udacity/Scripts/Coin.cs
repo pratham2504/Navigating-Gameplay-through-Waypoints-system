d11ed032b5e417b85dd8c3087f16f5c7b18e3154
Pratham <pratham2504@gmail.com>
2017-06-01 01:04:13 +0530

    First commit

---
+++ Assets/Udacity/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour 
+{
+	//Create a reference to the CoinPoofPrefab
+	public GameObject coinPoof;
+
+	public AudioSource coinAudio;
+
+    public void OnCoinClicked() {
+
+		//Audio
+		coinAudio.Play();
+
+        // Instantiate the CoinPoof Prefab where this coin is located
+		Instantiate (coinPoof, transform.position, Quaternion.Euler (-90, 0, 0));
+        // Make sure the poof animates vertically
+        // Destroy this coin. Check the Unity documentation on how to use Destroy
+		Destroy(gameObject, 0.5f);
+    }
+
+}
---
+++ Assets/Udacity/Scripts/Key.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour 
+{
+    //Create a reference to the KeyPoofPrefab and Door
+	public GameObject keyPoof;
+	public Door doorOpen;
+
+	//public Animation floatingKey;
+
+	public AudioSource keyAudio;
+
+	private bool KeyCollected = false;
+
+	void Update()
+	{
+		//Not required, but for fun why not try adding a Key Floating Animation here :)
+		//floatingKey.startPlayback();
+	}
+
+	public void OnKeyClicked()
+	{
+		//Audio
+		keyAudio.Play();
+
+        // Instatiate the KeyPoof Prefab where this key is located
+		Instantiate(keyPoof, transform.position, Quaternion.Euler(-90,0,0));
+        // Make sure the poof animates vertically
+        // Call the Unlock() method on the Door
+		doorOpen.Unlock();
+
+        // Set the Key Collected Variable to true
+		KeyCollected = true;
+        // Destroy the key. Check the Unity documentation on how to use Destroy
+		Destroy(gameObject, 0.7f);
+    }		
+}
